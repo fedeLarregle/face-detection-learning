e5fda4e0ae4062f7596ccdb139a170848e15cb38
DESKTOP-VMSGSAC\fedel <fede.larregle@gmail.com>
2018-03-31 16:43:01 -0300

    Setting up a basic Javafx GUI

---
+++ src/com/larregle/App.java
@@ -0,0 +1,10 @@
+package com.larregle;
+
+import com.larregle.facedetection.gui.MainApplication;
+import javafx.application.Application;
+
+public class App {
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class);
+    }
+}
---
+++ src/com/larregle/facedetection/gui/MainApplication.java
@@ -0,0 +1,100 @@
+package com.larregle.facedetection.gui;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
+
+public class MainApplication extends Application {
+
+    public static final String TITLE = "Face detection practice";
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+
+    private final Scene scene;
+    private final BorderPane borderPane;
+    private final FileChooser fileChooser;
+
+    private ImageView imageView;
+
+    public MainApplication() {
+        borderPane = new BorderPane();
+        scene = new Scene(borderPane, WIDTH, HEIGHT);
+        fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().setAll(new FileChooser.ExtensionFilter("Images", "*.jpg", "*.png"));
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle(TITLE);
+        borderPane.setTop(createMenuBar(primaryStage));
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    private MenuBar createMenuBar(Stage stage) {
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("File");
+        MenuItem loadItem = new MenuItem("Load Image");
+        MenuItem detectItem = new MenuItem("Detect faces");
+        MenuItem exitItem = new MenuItem("Exit");
+        fileMenu.getItems().addAll(loadItem, detectItem, exitItem);
+
+        loadItem.setOnAction(e -> {
+            File file;
+            String path;
+            try {
+                file = fileChooser.showOpenDialog(stage);
+                if (file != null) {
+                    path = file.toURI().toURL().toExternalForm();
+                    Image image = new Image(path, 700, 500, false, false);
+                    imageView = new ImageView();
+                    imageView.setFitWidth(700);
+                    imageView.setFitHeight(500);
+                    imageView.setPreserveRatio(true);
+                    imageView.setImage(image);
+                    borderPane.setCenter(imageView);
+                }
+            } catch (MalformedURLException e1) {
+                e1.printStackTrace();
+            }
+
+        });
+
+        detectItem.setOnAction(e -> {
+            // TODO start detection algorithm
+        });
+
+        exitItem.setOnAction(e -> {
+            if (quitAlert().showAndWait().get().equals(ButtonType.OK)) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        menuBar.getMenus().add(fileMenu);
+        return menuBar;
+    }
+
+
+
+    private Alert quitAlert() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Warning");
+        alert.setHeaderText(null);
+        alert.setContentText("Are you sure to quit?");
+        return alert;
+    }
+
+}
